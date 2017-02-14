4b05841198ac27542f02f2c33a3ca713c92cca9e
amitsrivastav <amitsri009@gmail.com>
2017-02-14 18:59:34 +0530

    updated OSBase metrics

--- script_examples/OSbasicMetrics.sh
+++ script_examples/OSbasicMetrics.sh
@@ -1,23 +1,21 @@
 
-############  XXX  XXXX    XX  XX
-############ XX    XX XX   XX  XX
-############ XX    XX  XX  XX  XX
-############ XX    XX XX   XX  XX
-############ XX    XXXX    XX  XX
-############ XX    XX      XX  XX
-############  XXX  XX       XXXX
+############  XXX  XXXX    XX  XX
+############ XX    XX XX   XX  XX
+############ XX    XX  XX  XX  XX
+############ XX    XX XX   XX  XX
+############ XX    XXXX    XX  XX
+############ XX    XX      XX  XX
+############  XXX  XX       XXXX
 
-# Capture the current CPU metrics 1 time with 1 second interval
-cpucmd=`sar -o ~/output.sar 1 1 | grep -v -e '^$' | grep -vwE "usr|Average"`
+# Capture the current CPU metrics 1 time with 1 second interval
+cpucmd=`sar 1 1 |grep Average`
 # Delete the temporary file created by the sar command
-`rm ~/output.sar`
 
 # Split the output string into its several components
-time=`echo "${cpucmd}" | awk '{ print $1 }'`
-usrcpu=`echo "${cpucmd}" | awk '{ print $2 }'`
-nicecpu=`echo "${cpucmd}" | awk '{ print $3 }'`
-syscpu=`echo "${cpucmd}" | awk '{ print $4 }'`
-idlecpu=`echo "${cpucmd}" | awk '{ print $5 }'`
+usrcpu=`echo "${cpucmd}" | awk '{ print $3 }'`
+nicecpu=`echo "${cpucmd}" | awk '{ print $4 }'`
+syscpu=`echo "${cpucmd}" | awk '{ print $5 }'`
+idlecpu=`echo "${cpucmd}" | awk '{ print $8 }'`
 
 # Test output, commented by default
 #echo Time    : ${time}
@@ -31,20 +29,21 @@ echo Metric:OSbasicMetrics/CPUUser/${usrcpu}[percentage]
 echo Metric:OSbasicMetrics/CPUSystem/${syscpu}[percentage]
 echo Metric:OSbasicMetrics/CPUIdle/${idlecpu}[percentage]
 
-############ XXX   XXX  XXXXX  XXX   XXX   XXXX   XXXX    XX  XX
-############ XXXX XXXX  XX     XXXX XXXX  XX  XX  XX XX    XXXX
-############ XX XXX XX  XX     XX XXX XX  XX  XX  XX  XX    XX
-############ XX     XX  XXXX   XX     XX  XX  XX  XX XX     XX
-############ XX     XX  XX     XX     XX  XX  XX  XXXX      XX
-############ XX     XX  XX     XX     XX  XX  XX  XX XX     XX
-############ XX     XX  XXXXX  XX     XX   XXXX   XX  XX    XX
+############ XXX   XXX  XXXXX  XXX   XXX   XXXX   XXXX    XX  XX
+############ XXXX XXXX  XX     XXXX XXXX  XX  XX  XX XX    XXXX
+############ XX XXX XX  XX     XX XXX XX  XX  XX  XX  XX    XX
+############ XX     XX  XXXX   XX     XX  XX  XX  XX XX     XX
+############ XX     XX  XX     XX     XX  XX  XX  XXXX      XX
+############ XX     XX  XX     XX     XX  XX  XX  XX XX     XX
+############ XX     XX  XXXXX  XX     XX   XXXX   XX  XX    XX
 
 ## NOTE!!! systemstats requires sudo access, so you need to make sure this script is executed by a user that is part
 ##         of the sudoers list. If prompted for password, the script will report empty memory metrics.
 
-memorystats=`sudo systemstats | grep -A 5 "Memory Summary" | grep 'Total\|Free'`
-totalmemory=`echo ${memorystats} | awk '{ print $2 }'`
-freememory=`echo ${memorystats} | awk '{ print $5 }'`
-usedmemory=`echo "${totalmemory}-${freememory}" | bc`
+memorystats=`free -m|grep Mem`
+totalmemory=`echo ${memorystats} | awk {'print $2'}`
+freememory=`echo ${memorystats} | awk {'print $3'}`
+usedmemory=`echo ${memorystats} |awk {'print$4'}`
 echo Metric:OSbasicMetrics/MemoryUsed/${usedmemory}[Megabytes]
 echo Metric:OSbasicMetrics/MemoryFree/${freememory}[Megabytes]
+
